35cf2b0050c0e254830f4b0413533619d0e8da62
Manoj Kumar Manivannan <manoemails@gmail.com>
2019-08-18 06:19:18 +0530

    added spring boot project

---
+++ src/main/java/com/clustercorp/ci/travis/TravisCiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.clustercorp.ci.travis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisCiExampleApplication.class, args);
+	}
+
+}
